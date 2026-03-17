ff77306843e81a5b0ac31927f94d0f0226e85221
Makanda Daniel <makandadaniel63@gmail.com>
2026-03-17 16:31:59 +0100

    Add files via upload

---
+++ javascript/Jscript.js
@@ -0,0 +1,11 @@
+ 
+ let menubt = document.getElementById("menubt")
+ menubt.addEventListener("click", function (){
+
+    if(menu.style.display=="none") {
+        menu.style.display="block"
+    } else {
+        menu.style.display="none"
+    }
+
+ })
